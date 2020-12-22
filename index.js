c294db9229ff0a055bb58048d4cc3d6bbb3707d7
Nick Wittwer <nick.wittwer@me.com>
2020-12-22 16:25:57 +0100

    Removed extra build step

--- index.js
+++ index.js
@@ -125,26 +125,26 @@ const runAction = () => {
 		}
 	}
 
-	log(`Building${release ? " and releasing" : ""} the Electron app…`);
-	const cmd = useVueCli ? "vue-cli-service electron:build" : "electron-builder";
-	for (let i = 0; i < maxAttempts; i += 1) {
-		try {
-			run(
-				`${useNpm ? "npx --no-install" : "yarn run"} ${cmd} --${platform} ${
-					release ? "--publish always" : ""
-				} ${args}`,
-				appRoot,
-			);
-			break;
-		} catch (err) {
-			if (i < maxAttempts - 1) {
-				log(`Attempt ${i + 1} failed:`);
-				log(err);
-			} else {
-				throw err;
-			}
-		}
-	}
+	// log(`Building${release ? " and releasing" : ""} the Electron app…`);
+	// const cmd = useVueCli ? "vue-cli-service electron:build" : "electron-builder";
+	// for (let i = 0; i < maxAttempts; i += 1) {
+	// 	try {
+	// 		run(
+	// 			`${useNpm ? "npx --no-install" : "yarn run"} ${cmd} --${platform} ${
+	// 				release ? "--publish always" : ""
+	// 			} ${args}`,
+	// 			appRoot,
+	// 		);
+	// 		break;
+	// 	} catch (err) {
+	// 		if (i < maxAttempts - 1) {
+	// 			log(`Attempt ${i + 1} failed:`);
+	// 			log(err);
+	// 		} else {
+	// 			throw err;
+	// 		}
+	// 	}
+	// }
 };
 
 runAction();
